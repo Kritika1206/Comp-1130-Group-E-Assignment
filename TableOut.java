3befa7ba11d18b3e27b08d3c3625cf729585ea5a
michaelB-comp <77687101+michaelB-comp@users.noreply.github.com>
2021-03-22 15:22:42 -0400

    Try

---
+++ TableOut.java
@@ -0,0 +1,21 @@
+public class TableOut {
+    public static void main(String[] args) {
+
+        System.out.println("Number Table ");
+        Table Table1 = new Table();
+        System.out.println(" : " + Table1.getNumbers());
+
+        // OPTION 2 COMMENTED OUT
+        // int height =8;
+        // int width = 6;
+
+
+        //System.out.println("Number Table :");
+
+        //   for (int i = 1; i < height; i++) {
+        // for (int k = 1; k <= width; k++) {
+        //     System.out.print( "|" + i * k + "| \t");
+        //  }
+        // System.out.println();
+    }
+}
