146f0ce292927f9bbe38f0011fd4a2994153c1e1
Kritika1206 <77856536+Kritika1206@users.noreply.github.com>
2021-03-23 11:21:25 -0700

    Fixed Table and The Circle Center

--- TableOut.java
+++ TableOut.java
@@ -1,22 +1,16 @@
 public class TableOut {
     public static void main(String[] args) {
 
-        System.out.println("Number Table ");
-        Table Table1 = new Table( );
-        System.out.println(" : " + Table1.getNumbers());
-
-        // OPTION 2 COMMENTED OUT
-        // int height =8;
-        // int width = 6;
-
-
-        //System.out.println("Number Table :");
-
-        //   for (int i = 1; i < height; i++) {
-        // for (int k = 1; k <= width; k++) {
-        //     System.out.print( "|" + i * k + "| \t");
-        //  }
-        // System.out.println();
+        System.out.println("Number Table:");
+        Table Table1 = new Table( 6 ,8,
+        "| 1  | 2  | 3  | 4  | 5  | 6  |" +"\n"+
+        "| 7  | 8  | 9  | 10 | 11 | 12 |" +"\n"+
+        "| 13 | 14 | 15 | 16 | 17 | 18 |" +"\n"+
+        "| 29 | 20 | 21 | 22 | 23 | 24 |" +"\n"+
+        "| 25 | 26 | 27 | 28 | 29 | 30 |" +"\n"+
+        "| 31 | 32 | 33 | 34 | 35 | 36 |" +"\n"+
+        "| 37 | 38 | 39 | 40 | 41 | 42 |" +"\n"+
+        "| 43 | 44 | 45 | 46 | 47 | 48 |");
+        System.out.println(Table1.getNumbers());
     }
 }
-//fix the program
--- TheCircleCenter.java
+++ TheCircleCenter.java
@@ -4,13 +4,13 @@ public class TheCircleCenter {
     public static void main(String[]args){
         int heightOfBlock, widthOfBlock;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the height:");
+        System.out.println("Enter the height:");    //scanning the height
         heightOfBlock = scan.nextInt();
-        System.out.println("Enter the width:");
+        System.out.println("Enter the width:");     //scanning the width
         widthOfBlock = scan.nextInt();
         
-    for (int i = -heightOfBlock; i < heightOfBlock; i++) {
-        for (int j = -widthOfBlock; j < widthOfBlock; j++) {
+    for (int i = -heightOfBlock; i < heightOfBlock; i++) {              //using the for loop to check the length
+        for (int j = -widthOfBlock; j < widthOfBlock; j++){             //using the for loop to check the width
             if (j * j + i * i <= heightOfBlock / 2 * widthOfBlock / 2) {
                 if (i == 0 && j == 0) {
                         System.out.print(" ");
